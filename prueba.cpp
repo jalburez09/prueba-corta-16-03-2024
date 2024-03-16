0c1e5d1ca033d6e750a1c75668543800b4296910
Andy Alburez <jalburez@umg.edu.gt>
2024-03-16 10:40:30 -0600

    Jose Alburez 0909-23-10025 segundo ejercicio

--- prueba.cpp
+++ prueba.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 //ejercicio 1
-int main() {
+/*int main() {
     int n;
     cout << "Ingrese la cantidad de numeros: ";
     cin >> n;
@@ -14,13 +14,38 @@ int main() {
         cin >> numeros[i];
     }
 
-
-
-
     cout<< "Lista ordenada de menor a mayor:\n";
     for (int i = 0; i < n; ++i) {
         cout << numeros[i] << " ";
-    }
+    }*/
+
+//segundo ejercicio//
+
+
+/*vector<int> miVector = {10, 20, 30, 40, 50};
+
+int numero;
+cout << "Ingrese un numero: ";
+cin >> numero;
+
+bool encontrado = false;
+int posicion = -1;
+
+for (int i = 0; i < miVector.size(); ++i) {
+if (miVector[i] == numero) {
+encontrado = true;
+posicion = i;
+break;
+}
+}
+
+if (encontrado) {
+cout << "El número " << numero << " está presente en el vector en la posición " << posicion << ".\n";
+} else {
+std::cout << "El número " << numero << " no se encuentra en el vector.\n";
+}*/
+
+
 
 
     return 0;
